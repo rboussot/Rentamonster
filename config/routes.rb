34b3985613f9ebd8c4dcafc8c21dde60c97360bb
Guillaume Laine <guillaumelaine2.0@gmail.com>
2016-08-16 19:07:32 +0200

    views and routes for basic search and index of monsters

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :monsters, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
+
 end
+
+
+
