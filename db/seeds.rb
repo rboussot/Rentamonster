5ab9ce2966b470ad5e77e1ffc4aecf5c2dab54f8
Romain Boussot <rboussot@gmail.com>
2016-08-16 16:46:53 +0200

    seeds for 3 users and their monsters

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+john = User.create(first_name: "John", last_name: "Lennon", password: "azerty", email: "john.lennon@gmail.com")
+david = User.create(first_name: "David", last_name: "Bowie", password: "azerty", email: "david.bowie@gmail.com")
+barack = User.create(first_name: "Barack", last_name: "Obama", password: "azerty", email: "barack.obama@gmail.com")
+Monster.create(name: "Bill", user:john, species:"Vampire")
+Monster.create(name: "Nessie", user:david, species:"Dinosaur")
+Monster.create(name: "Garou",user:barack, species:"Werewolf")
+
