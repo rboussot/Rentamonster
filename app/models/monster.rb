f5b867b3a6d94b7cbd3e3b01bac771aff17c8da9
Romain Boussot <rboussot@gmail.com>
2016-08-16 15:20:57 +0200

    validations

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,2 +1,7 @@
 class Monster < ApplicationRecord
+        has_many :bookings
+        belongs_to :user
+        validates :name, presence: true
+        validates :species, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+        has_many :monsters, dependent: :destroy
+        has_many :bookings, dependent: :destroy
+        validates :email, presence: true, uniqueness: true
+        validates :first_name, presence: true
+        validates :last_name, presence: true
+
 end
