b300f7277e2e78f383e6827498a750668bac093c
Romain Boussot <rboussot@gmail.com>
2016-08-16 18:12:52 +0200

    book a monster without style

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,24 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
+    @monster = Monster.find(params[:monster_id])
   end
 
   def create
+    @booking = Booking.save(booking_params)
+    if @booking.save
+      redirect_to monster_bookings_path
+    else
+      render :new
+    end
   end
 
   def index
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :price, :status, :user_id, :monster_id)
   end
 end
