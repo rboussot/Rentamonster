9a86d2e794222d123850d074ea5985a8dc66911e
Guillaume Laine <guillaumelaine2.0@gmail.com>
2016-08-18 13:27:31 +0200

    changed columns type from string to date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817111423) do
+ActiveRecord::Schema.define(version: 20160818111655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
